1bfe3b494939270237b5aab56db1aeaf7b96fa6d
Donard97 <donardgolaj@gmail.com>
2021-08-05 17:51:59 +0200

    Add javascript to validate email on form

--- main.js
+++ main.js
@@ -18,3 +18,25 @@ document.querySelectorAll('.menuList').forEach((item) => {
 
 closeIcon.addEventListener('click', menuClose);
 openIcon.addEventListener('click', menuOpen);
+
+
+const formValidation = document.querySelector('#form');
+const emailControl = document.querySelector('#email-address');
+
+const alertMessage = document.createElement('p');
+alertMessage.classList.add('error-Message');
+alertMessage.textContent = 'Your email should only contain lowercase letters.';
+
+const errorEmail = document.querySelector('.email-msg');
+errorEmail.appendChild(alertMessage);
+
+function validationFunct (e) {
+  if (emailControl.value !== emailControl.value.toLowerCase()) {
+    errorEmail.style.display = 'block';
+    e.preventDefault();
+  } else {
+    errorEmail.style.display = 'none';
+  }
+}
+
+formValidation.addEventListener('submit', validationFunct);
